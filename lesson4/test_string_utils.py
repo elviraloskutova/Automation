2cb432ebfcb5df64645aa04e4353e17d043f8d8d
Эльвира Лоскутова <elviraloskutova8@gmail.com>
2024-08-31 12:52:53 +0300

    fix problem, add comment

--- lesson4/test_string_utils.py
+++ lesson4/test_string_utils.py
@@ -84,6 +84,86 @@ def test_delete_symbol(string, symbol, result):
     res = utils.delete_symbol(string, symbol)
     assert res == result
 
+"""start _with"""
+
+@pytest.mark.parametrize("string, symbol, result", [
+
+    ("свет", "с", True),
+    ("", "", True),
+    ("Минск", "М", True),
+    ("Cinema ", "C", True),
+    ("Ира-Аня", "И", True),
+    ("123", '1', True),
+
+    ("Иван", "и", False),
+    ("мир", "М", False),
+    ("", "@", False),
+    ("зонт", "ж", False),
+    ("животное", "з", False)
+])
+def test_starts_witch(string, symbol, result):
+    res = utils.starts_with(string, symbol)
+    assert res == result
+
+"""end_with"""
+
+@pytest.mark.parametrize("string, symbol, result", [
+
+    ("Мария", "я", True),
+    ("СУП", "П", True),
+    ("", "", True),
+    ("кот ", "", True),
+    ("87", "7", True),
+    ("ОДИН-ДВа", "а", True),
+    ("Илья1", "1", True),
+    ("БаобаБ", "Б", True),
+    
+    ("лес", "п", False),
+    ("тигр", "с", False),
+    ("дверь", "Ь", False),
+    ("", "*", False)
+])
+def test_end_with(string, symbol, result):
+    res = utils.end_with(string, symbol)
+    assert res == result
+
+"""is_empty"""
+
+@pytest.mark.parametrize("string, result", [
+    ("", True),
+    (" ", True),
+    ("  ", True),
+
+    ("не пусто", False),
+    ("значение с пробелом", False),
+    ("123", False)
+])
+def test_is_empty(string, result):
+    res = utils.is_empty(string)
+    assert res == result
+
+"""list_to_string"""
+
+@pytest.mark.parametrize("lst, joiner, result", [
+   (["s", "o", "s"], ",", "s,o,s"),
+   ([1, 2, 3, 4, 5], None, "1, 2, 3, 4, 5"),
+   (["Первый", "Второй"], "-", "Первый-Второй"),
+   (["Первый", "Второй"], "Середина", "ПервыйСерединаВторой"),
+   (["в", "у", "з"], "", "вуз"),
+
+   ([], None, ""),
+   ([], ",", ""),
+   ([], "кот", "") 
+])
+def test_list_to_string(lst, joiner, result):
+    if joiner == None:
+        res = utils.list_to_string(lst)
+    else:
+        res = utils.list_to_string(lst, joiner)
+    assert res == result
+
+
+
     
 
 
